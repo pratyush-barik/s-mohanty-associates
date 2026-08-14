e9a865465826fc7ed18718726f08c8c4d85662f7
Pratyush Barik <scarlight.c39@gmail.com>
2026-08-14 17:59:54 +0530

    style: increase navbar logo size for readability

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -168,13 +168,13 @@ export default function Navbar({ user }: NavbarProps) {
         className="fixed top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-xl shadow-md border-b border-neutral-200/50 transition-all duration-500"
       >
         <div className="container mx-auto max-w-[1600px] pl-2 pr-6">
-          <div className="flex items-center justify-between h-[87px]">
+          <div className="flex items-center justify-between h-[90px] sm:h-[110px]">
             <Link href="/" className="flex items-center group">
               <div className="transition-all -ml-2">
                 <img
                   src="/logo/smohantyassociate_logo.png"
                   alt="S Mohanty Associates"
-                  className="h-[66px] sm:h-[79px] w-auto object-contain transition-all duration-300"
+                  className="h-[75px] sm:h-[100px] w-auto object-contain transition-all duration-300"
                 />
               </div>
             </Link>
