b54d48c7ca4c943f3dc970151859063a5e177bac
Pratyush Barik <scarlight.c39@gmail.com>
2026-09-11 11:20:03 +0530

    fix: place 'use client' at the very top of AdityaBirlaHousingHLLAP.tsx

--- src/app/portal/reports/[projectId]/banks/aditya-birla-housing/AdityaBirlaHousingHLLAP.tsx
+++ src/app/portal/reports/[projectId]/banks/aditya-birla-housing/AdityaBirlaHousingHLLAP.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 'use client';
+
+import React from 'react';
 import BankReportBuilder, { BankReportBuilderProps } from '../../BankReportBuilder';
 import { BankConfig, normalizeMapImages } from '@/lib/bank-fields';
 import { PDFAdityaBirlaHousingRenderer, HLLAPReportFields } from '@/lib/banks/pdf-aditya-birla-housing-renderer';
