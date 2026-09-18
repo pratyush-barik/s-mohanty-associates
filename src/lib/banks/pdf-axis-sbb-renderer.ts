41f74192d9eb4ea9d1b8723042179a8d64d14cf0
Ravi-667 <84ravikesharwani@gmail.com>
2026-09-18 19:43:10 +0530

    Revert "Remove Property Identification header in Axis SBB section 4"
    This reverts commit bcecc7e4d4891599f3c0767ebd85f080812c0b8d.

--- src/lib/banks/pdf-axis-sbb-renderer.ts
+++ src/lib/banks/pdf-axis-sbb-renderer.ts
@@ -114,6 +114,7 @@ export class PDFAxisSBBRenderer extends PDFBankRenderer {
     }
     // Intercept standard section 4
     if (title.toUpperCase() === 'PROPERTY IDENTIFICATION & POSTAL ADDRESS') {
+      super.drawSectionHeader('PROPERTY IDENTIFICATION & POSTAL ADDRESS', addSpaceBefore, preserveCase);
       this.drawSbbSection4();
       return;
     }
